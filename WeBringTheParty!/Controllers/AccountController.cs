8ce5f9d67f53b04ad80b3d425b67464713216624
Nikolas Krecu <nkrecu03@gmail.com>
2026-01-22 15:25:32 -0600

    added login and welcom page for user

---
+++ WeBringTheParty!/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeBringTheParty_.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login(Models.LoginViewModel model)
+        {
+            return RedirectToAction("Welcome", new {username = model.Username });
+        }
+
+        [HttpPost]
+        public IActionResult Logout() { 
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Welcome(string username)
+        {
+            ViewBag.Username = username;
+            return View();
+        }
+    }
+}
